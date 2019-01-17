9f84df003ab4a6c767d91210ff6e162fa5a98ab5
yauheniivashkevich <yauheni.ivashkevich@glympse.com>
2019-01-17 21:30:00 +0300

    updated lab5

--- lab5/Candies/Alenka.cs
+++ lab5/Candies/Alenka.cs
@@ -1,1 +1,11 @@
-﻿using System; namespace lab5.Candies {     public class Alenka : Candy     {         public Alenka() : base("Alenka", 1.1, 3.5)         {          }     } } 
+﻿using System; 
+namespace lab5.Candies 
+{     
+	public class Alenka : Candy     
+	{         
+		public Alenka() : base("Alenka", 1.1, 3.5)         
+		{          
+
+		}     
+	}
+ } 
